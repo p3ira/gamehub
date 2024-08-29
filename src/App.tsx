d4adb983a6118493b2b57a00d7ac86bced43b1f5
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-08-29 10:53:38 +0200

    Added img to genres

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenreList.tsx";
 
 function App() {
-    return <Grid templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-    }}>
+    return <Grid
+        templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}
+        templateColumns={{
+            base: '1fr',
+            lg: '200px 1fr'
+        }}
+    >
         <GridItem area='nav'>
             <NavBar />
         </GridItem>
         <Show above="lg">
-            <GridItem area='aside'>
+            <GridItem area='aside' paddingX={5}>
                 <GenreList />
             </GridItem>
         </Show>
