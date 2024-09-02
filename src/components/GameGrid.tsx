50c335b7d0fb1d388d449b540e4f8ff0532aa504
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-09-02 09:29:57 +0200

    Added filtering games by genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,23 +3,28 @@ import useGames from "../hooks/useGames.ts";
 import GameCard from "./CameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
 import GameCardContainer from "./GameCardContainer.tsx";
+import {Genre} from "../hooks/useGenres.ts";
+
+interface Props {
+    selectedGenre: Genre | null
+}
 
 
-const GameGrid = () => {
-    const {data, error, isLoading} = useGames();
+const GameGrid = ({ selectedGenre }: Props) => {
+    const {data, error, isLoading} = useGames(selectedGenre);
     const skeletons = [1, 2, 3, 4, 5, 6];
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={3}>
                 {isLoading && skeletons.map(skeleton =>
-                    <GameCardContainer>
-                        <GameCardSkeleton key={skeleton} />
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton />
                     </GameCardContainer>
                         )}
                 {data.map((game) => (
-                    <GameCardContainer>
-                        <GameCard key={game.id} game={game} />
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game} />
                     </GameCardContainer>
                 ))}
             </SimpleGrid>
