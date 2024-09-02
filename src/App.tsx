d5e4adef393bd472532c55014a53dd6e0b4391df
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-09-02 10:43:19 +0200

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ import {Genre} from "./hooks/useGenres.ts";
 import PlatformSelector from "./components/PlatformSelector.tsx";
 import {Platform} from "./hooks/useGames.ts";
 
+export interface GameQuery {
+    genre: Genre | null;
+    platform: Platform | null;
+}
+
 function App() {
-    const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-    const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+    const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
 
     return <Grid
         templateAreas={{
@@ -26,15 +30,15 @@ function App() {
         </GridItem>
         <Show above="lg">
             <GridItem area='aside' paddingX={5}>
-                <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) =>setSelectedGenre(genre)} />
+                <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) =>setGameQuery({...gameQuery, genre})} />
             </GridItem>
         </Show>
         <GridItem area='main'>
             <PlatformSelector
-                selectedPlatform={selectedPlatform}
-                onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}
             />
-            <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
+            <GameGrid gameQuery={gameQuery}/>
         </GridItem>
     </Grid>
 }
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 import useData from "./useData.ts";
-import {Genre} from "./useGenres.ts";
+import {GameQuery} from "../App.tsx";
 
 export interface Platform {
     id: number;
@@ -14,13 +14,13 @@ export interface Game {
     parent_platforms: {platform: Platform}[];
     metacritic: number;
 }
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) =>
+const useGames = (gameQuery: GameQuery) =>
     useData<Game>('/games', {
         params: {
-            genres: selectedGenre?.id,
-            platforms: selectedPlatform?.id
+            genres: gameQuery.genre?.id,
+            platforms: gameQuery.platform?.id
         }},
-        [selectedGenre?.id, selectedPlatform?.id]
+        [gameQuery]
     );
 
 export default useGames
