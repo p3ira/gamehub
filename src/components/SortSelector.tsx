2d636bd8d26fdd2c44944fe2817ecc03ab3c85ad
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-09-02 10:58:16 +0200

    Implemented ordering by sort

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,37 @@
+import {Button, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {BsChevronDown} from "react-icons/bs";
+
+interface Props {
+    onSelectSortOrder: (sortOrder: string) => void;
+}
+const SortSelector = ({onSelectSortOrder}: Props )=> {
+    const sortOrders = [
+        { value: '', label: 'Relevance'},
+        { value: '-added', label: 'Date Added'},
+        { value: 'name', label: 'Name'},
+        { value: '-released', label: 'Release Date'},
+        { value: '-metacritic', label: 'Popularity'},
+        { value: '-rating', label: 'Average rating'},
+    ]
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+                Order By: Relevance
+            </MenuButton>
+            <MenuList>
+                {sortOrders.map((order) => (
+                    <MenuItem
+                        onClick={() => onSelectSortOrder(order.value)}
+                        key={order.value}
+                        value={order.value}
+                    >
+                        {order.label}
+                    </MenuItem>)
+                )}
+            </MenuList>
+        </Menu>
+    )
+}
+
+export default SortSelector
