dc72290e538cfb03b23438fe62b9bcfae1884fe7
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-09-26 11:34:39 +0200

    Implemented infite query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
 import {GameQuery} from "../App.tsx";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client.ts";
 import { Platform } from "./usePlatforms.ts";
 
@@ -14,16 +14,21 @@ export interface Game {
     rating_top: number;
 }
 const useGames = (gameQuery: GameQuery) =>
-    useQuery<FetchResponse<Game>, Error>({
+    useInfiniteQuery<FetchResponse<Game>, Error>({
         queryKey: ['games', gameQuery],
-        queryFn: () => apiClient.getAll({
+        queryFn: ({ pageParam = 1}) => apiClient.getAll({
            params: {
                 genres: gameQuery.genre?.id,
                 parent_platforms: gameQuery.platform?.id,
                 ordering: gameQuery.sortOrder,
                 search: gameQuery.searchText,
+                page: pageParam,
             }, 
         }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.next ? allPages.length + 1 : undefined;
+        },
+        initialPageParam: 0,
     })
 
 export default useGames
