de6eb665dc457b125a1fc0c023caeafe5b33d24b
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-09-26 10:32:12 +0200

    refactoring, removed duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,40 +1,40 @@
 import {
-    FaWindows,
-    FaPlaystation,
-    FaXbox,
-    FaApple,
-    FaLinux,
-    FaAndroid
-} from 'react-icons/fa';
-import { MdPhoneIphone} from "react-icons/md";
-import {SiNintendo} from "react-icons/si";
-import {BsGlobe} from "react-icons/bs";
-import {Platform} from "../hooks/useGames.ts";
-import {HStack, Icon} from "@chakra-ui/react";
-import {IconType} from "react-icons";
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/usePlatforms.ts";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
 interface Props {
-    platforms: Platform[]
+  platforms: Platform[];
 }
-const PlatformIconList = ({platforms}: Props) => {
-    const iconMap: { [key:string]: IconType } = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        nintendo: SiNintendo,
-        mac: FaApple,
-        linux: FaLinux,
-        ios: MdPhoneIphone,
-        web: BsGlobe,
-        android: FaAndroid
-    }
-    return (
-        <HStack marginY={1}>
-            {platforms.map((platform) =>
-                <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
-            )}
-        </HStack>
-    )
-}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
 
-export default PlatformIconList
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,7 @@
 import {GameQuery} from "../App.tsx";
 import { useQuery } from "@tanstack/react-query";
 import apiClient, { FetchResponse } from "../services/api-client.ts";
-
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
+import { Platform } from "./usePlatforms.ts";
 
 export interface Game {
     id: number;
