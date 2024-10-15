54be1f81c7819e2cf9616add3bfb6812c1a516ed
Per Erik Kattai-Eira <per.erik.eira@gmail.com>
2024-10-15 10:24:55 +0200

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandebleText from "../components/ExpandebleText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
       <ExpandebleText>{game.description_raw}</ExpandebleText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
